eefca612e50e3301d474ee522c6357a28f5d2c6b
FamaLine-dev <famalineid@gmail.com>
2026-06-19 02:09:50 +0700

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p app/src/main/java/com/vortexstore/api
+mkdir -p app/src/main/java/com/vortexstore/models
+mkdir -p app/src/main/java/com/vortexstore/services
+mkdir -p app/src/main/java/com/vortexstore/ui
+mkdir -p app/src/main/java/com/vortexstore/ui/adapters
+mkdir -p app/src/main/java/com/vortexstore/utils
